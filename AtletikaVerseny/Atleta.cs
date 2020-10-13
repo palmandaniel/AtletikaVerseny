7e0b12ac4af8377cef061b99c55ab21b0d5637be
palmandaniel <palman.daniel47@gmail.com>
2020-10-13 08:55:26 +0200

    Kesz feladat, nevek egyben

---
+++ AtletikaVerseny/Atleta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+
+        public string ANeve { get; private set; }
+        public Atleta(string szoveg)
+        {
+            string[] adat = szoveg.Split(';');
+            Rajtszam = adat[0];
+            //Atleta.Nev(adat[1], VezNev, KerNev);
+            ANeve = adat[1];
+            Egyesulet = adat[2];
+            Ugras = int.Parse(adat[3]);
+        }
+
+        public static void Nev(string nev, string VezNev, string KerNev)
+        {
+            nev.Split(' ');
+            VezNev = Convert.ToString(nev[0]);
+            KerNev = Convert.ToString(nev[1]);
+        }
+    }
+}
--- AtletikaVerseny/Program.cs
+++ AtletikaVerseny/Program.cs
@@ -3,14 +3,115 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AtletikaVerseny
 {
     class Program
     {
+        static List<Atleta> atletak = new List<Atleta>();
+        static List<string> egyesuletek = new List<string>();
+        static void Feladat1()
+        {
+            Console.WriteLine("1. Feladat: adatok beolvasása...\n");
+            StreamReader file = new StreamReader("tavol.csv");
+            while (!file.EndOfStream)
+            {
+                atletak.Add(new Atleta(file.ReadLine()));
+            }
+            file.Close();
+        }
+
+        static void Feladat2()
+        {
+            Console.WriteLine("2. Feladat: Nevek és ugrások");
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                Console.WriteLine($"\t {atletak[i].ANeve}\t {atletak[i].Ugras}");
+            }
+        }
+
+        static void Feladat3()
+        {
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                if (!egyesuletek.Contains(atletak[i].Egyesulet))
+                {
+                    egyesuletek.Add(atletak[i].Egyesulet);
+                }
+            }
+
+            Console.WriteLine("\n3. Feladat: Egyesületek");
+            for (int i = 0; i < egyesuletek.Count; i++)
+            {
+                Console.WriteLine(egyesuletek[i]);
+            }
+        }
+
+        static void Feladat4()
+        {
+            Console.WriteLine("\n4. Feladat: Legnagyobb ugrás");
+            int legnagyobb = 0;
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                if (legnagyobb<atletak[i].Ugras)
+                {
+                    legnagyobb = atletak[i].Ugras;
+                }
+            }
+
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                if (atletak[i].Ugras == legnagyobb)
+                {
+                    Console.WriteLine($"{atletak[i].ANeve}: {atletak[i].Ugras} cm");
+                }
+            }
+        }
+
+        static void Feladat5()
+        {
+            Console.WriteLine();
+            int db = 0;
+            double atlag = 0;
+            double sum = 0;
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                sum += atletak[i].Ugras;
+            }
+
+            atlag = sum / atletak.Count;
+
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                if (atletak[i].Ugras < atlag)
+                {
+                    db++;
+                }
+            }
+
+            Console.WriteLine("5. Feladat: Átlag alatt lévő ugrások száma: {0}",db);
+        }
+
+        static void Feladat6()
+        {
+            Console.WriteLine("\n6. Feladat: Adatok fájlba írása...");
+            StreamWriter fileba = new StreamWriter("versenyzok.txt");
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                fileba.WriteLine($"{atletak[i].Rajtszam};{atletak[i].ANeve}");
+            }
+            fileba.Close();
+        }
         static void Main(string[] args)
         {
-
+            Feladat1();
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+            Feladat6();
+            Console.ReadKey();
         }
     }
 }
